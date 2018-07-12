aea8f6837e5541f898f244dc72e23cc70d3b9368
Federico Pazos <fede.pazos95@gmail.com>
2018-07-11 22:48:56 -0300

    fixes

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ express()
     let result = ''
     const times = process.env.TIMES || 5
     for (i = 0; i < times; i++) {
-      result += i + ' ' 
+      result += i + ' '
     }
     res.send(result)
   })
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table')
-      res.render('pages/db', result)
-      client.release()
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      client.release();
     } catch (err) {
-      console.log(err)
-      res.send('Error ' + err)
+      console.error(err);
+      res.send("Error " + err);
     }
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
