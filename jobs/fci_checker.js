efe1714326b8843f6510a4be882f7eee7015c169
Federico Pazos <fede.pazos95@gmail.com>
2018-07-11 23:36:29 -0300

    testing fci checker job

--- jobs/fci_checker.js
+++ jobs/fci_checker.js
@@ -1,5 +1,39 @@
-const sayHello = () => {
-    console.log('Hello');
+const axios = require('axios');
+const queryString = require('query-string');
+const { Pool } = require('pg');
+const url = 'https://www.fondosalpha.com.ar/alpha/comparador-fondos-p?';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+console.log(process.env);
+const checkFci = (fci) => {
+    axios
+    .post(url, queryString.stringify({
+		option: '105',
+		periodo: 'periodoMes',
+		fdesde: '',
+		fhasta: '',
+		variable: 'variableVariacion',
+		visual: 'visualGrafico'
+    }))
+    .then(async (res) => {
+        const fci = {
+            name: res.data[0].name,
+            var: res.data[0].data[res.data[0].data.length - 1]
+        }
+        console.log(fci);
+        try {
+            const client = await pool.connect()
+            const result = await client.query('SELECT * FROM test_table');
+            console.log(result.rows);
+            client.release();
+          } catch (err) {
+            console.error(err);
+          }
+    })
 }
 
-sayHello();
+checkFci();
