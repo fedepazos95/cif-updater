59550321c1ba8a7e03f1d6cdcaf7e62a82c25181
Federico Pazos <fede.pazos95@gmail.com>
2018-07-12 22:42:56 -0300

    fix in loadaccounts wallet

--- Wallet.js
+++ Wallet.js
@@ -4,10 +4,11 @@ const keys = require('./config/keys');
 module.exports = {
     listAccounts: async (token, user) => {
         try {
-            const response = await axios.get(`keys.budgetBakersUrl/${accounts}`, { headers: { 'X-Token': token, 'X-User': user } });
+            const response = await axios.get(`${keys.budgetBakersUrl}/accounts`, { headers: { 'X-Token': token, 'X-User': user } });
+            console.log('try list accounts', response);
             return response;
         } catch (err) {
-            console.error(err);
+            console.error('catch list accounts', err);
         }
     }
 }
