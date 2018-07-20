74188b1424d83428823ecb0c3dba6a56815fd0f9
Fede <fede.pazos95@gmail.com>
2018-07-20 03:09:47 -0300

    Delete dev.js

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    walletToken: 'c77855ca-8584-4652-88b4-764c838945e4',
-    user: 'fede.pazos95@gmail.com',
-    icbcUrl: 'https://www.fondosalpha.com.ar/alpha/comparador-fondos-p?',
-    budgetBakersUrl: 'https://api.budgetbakers.com/api/v1',
-    currencyId: 'eec17219-4748-4a22-a009-060e4a3f4d1f',
-    categoryId: '34f65442-9a52-4f37-a194-33f3af4dffd7'
-}
