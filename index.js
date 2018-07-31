5d7f3aa31941f69f9cd9c0c0df6b145258002fb6
Federico Pazos <fede.pazos95@gmail.com>
2018-07-31 14:22:07 -0300

    frontend disabled

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => res.status(503).send('Service Unavailable')); //Response to disable the front-end side
+// app.get('/', (req, res) => res.render('pages/index'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
